c5aa3427294a626e98b10879d16195c86e7ceac6
unknown <nourhenjbali1@gmail.com>
2023-11-26 23:53:46 +0100

    comment

--- test/auth.test.js
+++ test/auth.test.js
@@ -7,22 +7,22 @@ import User from "../models/User.js";
 describe("Auth Controller", () => {
   it("should login a user", async () => {
     // Mocking the User.findOne method using Sinon
-    /*sinon.stub(User, "findOne").resolves({
+    sinon.stub(User, "findOne").resolves({
       username: "nourhen1",
       password: "123", // Replace with the actual hashed password
       isAdmin: true,
-    });*/
+    });
 
     const response = await request(app).post("/api/auth/login").send({
       username: "nourhen1",
       password: "123",
     });
-   // console.log("Response:", response.body); // Log the response body
+     console.log("Response:", response.body); // Log the response body
     console.log(response.statusCode);
     assert.strictEqual(response.statusCode, 200);
     assert.ok(response.body.details);
     assert.strictEqual(response.body.isAdmin, true);
-    //assert.ok(response.header["set-cookie"][0].startsWith("access_token="));
+    assert.ok(response.header["set-cookie"][0].startsWith("access_token="));
 
     // Restore the original User.findOne method
     // User.findOne.restore();
